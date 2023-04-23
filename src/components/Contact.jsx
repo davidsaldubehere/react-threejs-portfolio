fbdb975be3e99fc94d50a4df066e44941927cb48
David Saldubehere <davesaldub@gmail.com>
2023-04-22 21:21:16 -0400

    netlify form inegration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,17 +21,16 @@ const Contact = () => {
         <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form
-          //ref={formRef}
-          // onSubmit={handleSubmit}
+        name="contact"
+          method="POST"
           className='mt-12 flex flex-col gap-8'
         >
+          <input type="hidden" name="form-name" value="contact" />
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
             <input
               type='text'
               name='name'
-              //value={form.name}
-              //onChange={handleChange}
               placeholder="What's your name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
@@ -41,8 +40,6 @@ const Contact = () => {
             <input
               type='email'
               name='email'
-              //value={form.email}
-              //onChange={handleChange}
               placeholder="What's your email address?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
@@ -52,13 +49,10 @@ const Contact = () => {
             <textarea
               rows={7}
               name='message'
-              //value={form.message}
-              //onChange={handleChange}
               placeholder='What you want to say?'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-
           <button
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
